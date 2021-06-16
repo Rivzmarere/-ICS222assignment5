8b24512b5c34c13684cd53de3c2af91bdf2f787c
rivaldomarere <rivzmarere@gmail.com>
2021-06-16 10:37:22 +0200

    first

---
+++ assignemnt5.js
@@ -0,0 +1,12 @@
+function turnZebra() {
+    let rows = document.querySelectorAll('table tr:nth-child(odd)');
+    rows.forEach((row) => {
+        row.classList.add('zebraStripe');
+    });  } 
+
+    function resettable() {
+        let rows = document.querySelectorAll('table tr:nth-child(odd)');
+        rows.forEach((row) => {
+            row.classList.remove('zebraStripe');
+        });   
+    }
